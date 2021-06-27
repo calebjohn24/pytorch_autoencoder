2f40fb499fe07c831a771bd1c47b3b8e10715f64
Caleb John <cajohn0205@gmail.com>
2021-06-27 15:41:06 -0700

    finsihed datatloader

--- main.py
+++ main.py
@@ -6,4 +6,8 @@ import matplotlib.pyplot as plt
 
 
 transform = transforms.ToTensor()
-mnist_data = datasets.MNIST(root="./data", train=True, transform=transform)
+mnist_data = datasets.MNIST(root="./data", download=True, train=True, transform=transform)
+data_loader = torch.utils.data.DataLoader(dataset=mnist_data, batch_size=32, shuffle=True)
+data_iter = iter(data_loader)
+images, labels = data_iter.next()
+print(torch.min(images), torch.max(images))
