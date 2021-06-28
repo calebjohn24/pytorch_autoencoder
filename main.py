a3036c36516d1ecbd5a0ad564bf272ad174c0b59
Caleb John <cajohn0205@gmail.com>
2021-06-27 21:25:01 -0700

    updated for cifar dataset

--- main.py
+++ main.py
@@ -4,16 +4,39 @@ import torch.nn as nn
 import torch.optim as optim
 import matplotlib.pyplot as plt
 from  model import Autoencoder
+import numpy as np
 
 
 
-
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 transform = transforms.ToTensor()
-mnist_data = datasets.MNIST(root="./data", download=True, train=True, transform=transform)
-data_loader = torch.utils.data.DataLoader(dataset=mnist_data, batch_size=10, shuffle=True)
-data_iter = iter(data_loader)
-images, labels = data_iter.next()
-print(torch.min(images), torch.max(images))
+train_data = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+test_data = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+train_loader = torch.utils.data.DataLoader(train_data, batch_size=32, num_workers=0)
+test_loader = torch.utils.data.DataLoader(test_data, batch_size=32, num_workers=0)
+
+#Utility functions to un-normalize and display an image
+def imshow(img):
+    img = img / 2 + 0.5  
+    plt.imshow(np.transpose(img, (1, 2, 0))) 
+
+ 
+#Define the image classes
+classes = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+
+#Obtain one batch of training images
+dataiter = iter(train_loader)
+images, labels = dataiter.next()
+images = images.numpy() # convert images to numpy for display
+
+#Plot the images
+fig = plt.figure(figsize=(8, 8))
+# display 20 images
+for idx in np.arange(9):
+    ax = fig.add_subplot(3, 3, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
+
 model = Autoencoder()
 
 criterion = nn.MSELoss()
@@ -22,13 +45,18 @@ optimizer = torch.optim.Adam(model.parameters(),
                              weight_decay=1e-5)
 
 
-num_epochs = 8
+model = Autoencoder().to(device)
+
+criterion = nn.MSELoss()
+optimizer = torch.optim.Adam(model.parameters(),
+                             lr=1e-3, 
+                             weight_decay=1e-5)
+num_epochs = 20
 outputs = []
 for epoch in range(num_epochs):
-    for (img, _) in data_loader:
-        # img = img.reshape(-1, 28*28) # -> use for Autoencoder_Linear
-        recon = model(img)
-        loss = criterion(recon, img)
+    for (img, _) in train_loader:
+        recon = model(img.to(device))
+        loss = criterion(recon, img.to(device))
         
         optimizer.zero_grad()
         loss.backward()
@@ -38,24 +66,31 @@ for epoch in range(num_epochs):
     outputs.append((epoch, img, recon))
     print(f'Epoch:{epoch+1}, Loss:{loss.item():.4f}')
 
-print("done")
-for output in outputs:
-    plt.figure(figsize=(9, 2))
-    plt.gray()
-    imgs = output[1].detach().numpy()
-    recon = output[2].detach().numpy()
-    for i, item in enumerate(imgs):
-        if i >= 9: break
-        plt.subplot(2, 9, i+1)
-        # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
-        # item: 1, 28, 28
-        plt.imshow(item[0])
-            
-    for i, item in enumerate(recon):
-        if i >= 9: break
-        plt.subplot(2, 9, 9+i+1) # row_length + i + 1
-        # item = item.reshape(-1, 28,28) # -> use for Autoencoder_Linear
-        # item: 1, 28, 28
-        plt.imshow(item[0])
+#Batch of test images
+dataiter = iter(test_loader)
+images, labels = dataiter.next()
 
+#Sample outputs
+output = model(images.to(dev)).cpu()
+images = images.numpy()
+
+output = output.view(32, 3, 32, 32)
+output = output.detach().numpy()
+
+#Original Images
+print("Original Images")
+fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+for idx in np.arange(5):
+    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+    imshow(images[idx])
+    ax.set_title(classes[labels[idx]])
 plt.show()
+
+#Reconstructed Images
+print('Reconstructed Images')
+fig, axes = plt.subplots(nrows=1, ncols=5, sharex=True, sharey=True, figsize=(12,4))
+for idx in np.arange(5):
+    ax = fig.add_subplot(1, 5, idx+1, xticks=[], yticks=[])
+    imshow(output[idx])
+    ax.set_title(classes[labels[idx]])
+plt.show() 
--- model.py
+++ model.py
@@ -1,24 +1,25 @@
 import torch.nn as nn
 import torch.optim as optim
 
+
 class Autoencoder(nn.Module):
     def __init__(self):
         super().__init__()
         self.encoder = nn.Sequential(
-            nn.Conv2d(1, 16, 3, stride=2, padding=1), # -> N, 16, 14, 14
+            nn.Conv2d(3, 16, 3, stride=2, padding=1), # -> N, 16, 14, 14
             nn.ReLU(),
             nn.Conv2d(16, 32, 3, stride=2, padding=1), # -> N, 32, 7, 7
             nn.ReLU(),
-            nn.Conv2d(32, 64, 7) # -> N, 64, 1, 1
+            nn.Conv2d(32, 64, 2) # -> N, 64, 1, 1
         )
         
         self.decoder = nn.Sequential(
-            nn.ConvTranspose2d(64, 32, 7), # -> N, 32, 7, 7
+            nn.ConvTranspose2d(64, 32, 2), # -> N, 32, 7, 7
             nn.ReLU(),
             nn.ConvTranspose2d(32, 16, 3, stride=2, padding=1, output_padding=1), # N, 16, 14, 14 (N,16,13,13 without output_padding)
             nn.ReLU(),
-            nn.ConvTranspose2d(16, 1, 3, stride=2, padding=1, output_padding=1), # N, 1, 28, 28  (N,1,27,27)
-            nn.Sigmoid()
+            nn.ConvTranspose2d(16, 3, 3, stride=2, padding=1, output_padding=1), # N, 1, 28, 28  (N,1,27,27)
+            nn.ReLU()
         )
 
     def forward(self, x):
